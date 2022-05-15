161c94e17c6c7a77a5845c0f198b819c4759523e
PraneethJain <praneethjain005@gmail.com>
2022-05-15 19:57:03 +0530

    Update project structure

--- src/main.py
+++ src/main.py
@@ -1,17 +1,30 @@
 import sys
-from button import *
+from settings import *
+from button import Button
 
 
-def handle_events(events: list) -> None:
-    for event in events:
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
+class Window:
+    def __init__(self):
+        self.button = Button("Click me")
 
-button = Button("Play")
-while True:
-    handle_events(pg.event.get())
+    def run(self):
+        while True:
+            screen.fill((0, 128, 128))
+            self.handle_events(pg.event.get())
+            self.button.update()
+            pg.display.update()
 
-    screen.fill("darkblue")
-    button.update()
-    pg.display.flip()
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+
+def main():
+    window = Window()
+    window.run()
+
+
+if __name__ == "__main__":
+    main()
