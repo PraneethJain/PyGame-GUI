f6a11d0aec552f88e66bba9045e5eed9560068ca
PraneethJain <praneethjain005@gmail.com>
2022-05-26 08:50:49 +0530

    Changed version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygamesimplegui',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     author="Praneeth Jain",
     author_email='praneethjain005@gmail.com',
