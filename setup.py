30adbfc9e653f12a4929054872cf8dce0f7baac9
PraneethJain <praneethjain005@gmail.com>
2022-05-26 08:57:00 +0530

    Configured __init__.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygamesimplegui',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     author="Praneeth Jain",
     author_email='praneethjain005@gmail.com',
